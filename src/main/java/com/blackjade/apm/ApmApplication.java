d5be14a17f8b61b671a5e9ebdd1071c0473ae12d
Derek Fang <fangxianguk@sina.com>
2018-07-10 15:57:12 +0800

    update

--- src/main/java/com/blackjade/apm/ApmApplication.java
+++ src/main/java/com/blackjade/apm/ApmApplication.java
@@ -3,7 +3,9 @@ package com.blackjade.apm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.blackjade.apm.dao")
 public class ApmApplication {
